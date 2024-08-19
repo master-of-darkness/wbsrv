d11fb6d5def928d970792172d5545a27d6d54889
an <super.vava23.z@gmail.com>
2024-08-19 20:24:06 +0300

    custom handler

--- src/main.c
+++ src/main.c
@@ -1,18 +1,14 @@
-#include <cerrno>
-#include <climits>
+#include <errno.h>
+#include <limits.h>
 #include <netinet/in.h>
-#include <csignal>
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <filesystem>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include "h2o.h"
 
+#include "h2o.h"
 #include "h2o/memcached.h"
 #include "defines.h"
-#include "concurrent_lru_cache.h"
 
 #define USE_HTTPS 0
 #define USE_MEMCACHED 0
@@ -27,12 +23,10 @@ struct rowCache {
     const char* content_text;
 };
 
-static ConcurrentLRUCache<const char*, const char*> docCache(256);
-ConcurrentLRUCache<const char*, const char*>::ConstAccessor const_accessor;
-static const char *default_index_files[] = {"index.html", "index.htm", "index.txt", nullptr};
 
-static int setup_ssl(const char *cert_file, const char *key_file, const char *ciphers)
-{
+static const char *default_index_files[] = {"index.html", "index.htm", "index.txt", NULL};
+
+static int setup_ssl(const char *cert_file, const char *key_file, const char *ciphers) {
     SSL_load_error_strings();
     SSL_library_init();
     OpenSSL_add_all_algorithms();
@@ -42,8 +36,9 @@ static int setup_ssl(const char *cert_file, const char *key_file, const char *ci
 
     if (USE_MEMCACHED) {
         accept_ctx.libmemcached_receiver = &libmemcached_receiver;
-        h2o_accept_setup_memcached_ssl_resumption(h2o_memcached_create_context("127.0.0.1", 11211, 0, 1, "h2o:ssl-resumption:"),
-                                                  86400);
+        h2o_accept_setup_memcached_ssl_resumption(
+            h2o_memcached_create_context("127.0.0.1", 11211, 0, 1, "h2o:ssl-resumption:"),
+            86400);
         h2o_socket_ssl_async_resumption_setup_ctx(accept_ctx.ssl_ctx);
     }
 
@@ -68,55 +63,51 @@ static int setup_ssl(const char *cert_file, const char *key_file, const char *ci
     return 0;
 }
 
-
-static void on_accept(uv_stream_t *listener, int status)
-{
+static void on_accept(uv_stream_t *listener, int status) {
     uv_tcp_t *conn;
     h2o_socket_t *sock;
 
     if (status != 0)
         return;
 
-    conn = static_cast<uv_tcp_t *>(h2o_mem_alloc(sizeof(*conn)));
+    conn = h2o_mem_alloc(sizeof(*conn));
     uv_tcp_init(listener->loop, conn);
 
-    if (uv_accept(listener, reinterpret_cast<uv_stream_t *>(conn)) != 0) {
-        uv_close(reinterpret_cast<uv_handle_t *>(conn), reinterpret_cast<uv_close_cb>(free));
+    if (uv_accept(listener, (uv_stream_t *) conn) != 0) {
+        uv_close((uv_handle_t *) conn, (uv_close_cb) free);
         return;
     }
 
-    sock = h2o_uv_socket_create(reinterpret_cast<uv_handle_t *>(conn), reinterpret_cast<uv_close_cb>(free));
+    sock = h2o_uv_socket_create((uv_handle_t *) conn, (uv_close_cb) free);
     h2o_accept(&accept_ctx, sock);
 }
 
-static int create_listener()
-{
+static int create_listener(void) {
     static uv_tcp_t listener;
-    sockaddr_in addr{};
+    struct sockaddr_in addr;
     int r;
 
     uv_tcp_init(ctx.loop, &listener);
     uv_ip4_addr("127.0.0.1", 7890, &addr);
-    if ((r = uv_tcp_bind(&listener, reinterpret_cast<struct sockaddr *>(&addr), 0)) != 0) {
+    if ((r = uv_tcp_bind(&listener, (struct sockaddr *) &addr, 0)) != 0) {
         fprintf(stderr, "uv_tcp_bind:%s\n", uv_strerror(r));
         goto Error;
     }
-    if ((r = uv_listen(reinterpret_cast<uv_stream_t *>(&listener), 128, on_accept)) != 0) {
+    if ((r = uv_listen((uv_stream_t *) &listener, 128, on_accept)) != 0) {
         fprintf(stderr, "uv_listen:%s\n", uv_strerror(r));
         goto Error;
     }
 
     return 0;
 Error:
-    uv_close(reinterpret_cast<uv_handle_t *>(&listener), nullptr);
+    uv_close((uv_handle_t *) &listener, NULL);
     return r;
 }
 
 
-int main()
-{
+int main() {
     h2o_hostconf_t *hostconf;
-    h2o_access_log_filehandle_t *logfh = h2o_access_log_open_handle("/dev/stdout", nullptr, H2O_LOGCONF_ESCAPE_APACHE);
+    h2o_access_log_filehandle_t *logfh = h2o_access_log_open_handle("/dev/stdout", NULL, H2O_LOGCONF_ESCAPE_APACHE);
     h2o_pathconf_t *pathconf;
 
     signal(SIGPIPE, SIG_IGN);
@@ -125,8 +116,8 @@ int main()
     h2o_config_init(&config);
     hostconf = h2o_config_register_host(&config, h2o_iovec_init(H2O_STRLIT("default")), 65535);
     pathconf = h2o_config_register_path(hostconf, "/", 0);
-    h2o_file_register(pathconf, "examples/doc_root", nullptr, nullptr, 0);
-    if (logfh != nullptr)
+    wbsrv_file_register(pathconf, "docroot/", NULL, NULL, 0);
+    if (logfh != NULL)
         h2o_access_log_register(pathconf, logfh);
 
     uv_loop_t loop;
